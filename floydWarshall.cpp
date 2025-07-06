3301296864e941ab0c41d2b21b10cfcdb4103aef
Siddharth Shukla <formaledusiddh371@gmail.com>
2025-07-06 08:23:05 +0530

    added floydWarshall

---
+++ floydWarshall.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    void floydWarshall(int V,vector<vector<int>>&grid){
+        int n=grid.size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==-1){
+                    grid[i][j]=1000000;
+                }
+            }
+        }
+
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    grid[i][j]=min(grid[i][j],grid[i][via]+grid[via][j]);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1000000){
+                    grid[i][j]=-1;
+                }
+            }
+        }
+    }
+};
